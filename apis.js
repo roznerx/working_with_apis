324bc5dad50b153e1d40c36a350733d779f6d5b9
roznerx <ignaciorosner@gmail.com>
2022-04-08 18:16:02 -0300

    Code refactored in order to use async and await

--- apis.js
+++ apis.js
@@ -3,7 +3,37 @@ const refreshBtn = document.getElementById('refresh-btn');
 const searchBar = document.getElementById('search-bar');
 const goBtn = document.getElementById('go-btn');
 
-function fetchImg(url) {
+async function fetchImg(url) {
+    const response = await fetch(url, {mode: 'cors'});
+    const imgData = await response.json();
+    img.src = imgData.data.images.original.url;
+}
+
+fetchImg('https://api.giphy.com/v1/gifs/translate?api_key=cGpZJBFIYDVNjGEQ0HrqdINRPwTex42H&s=cats');
+
+refreshBtn.addEventListener('click', () => {
+    fetchImg('https://api.giphy.com/v1/gifs/translate?api_key=cGpZJBFIYDVNjGEQ0HrqdINRPwTex42H&s=cats');
+})
+
+
+goBtn.addEventListener('click', async () => {
+    try {
+        const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=cGpZJBFIYDVNjGEQ0HrqdINRPwTex42H&s=${searchBar.value}`, {mode: 'cors'});
+        if (response.ok) {
+                imgData = await response.json();
+                img.src = imgData.data.images.original.url;
+        } else 
+            throw new Error('That kind of gif might not exist!');
+    }
+    catch (error) {
+        alert('That kind of gif might not exist!');
+    }
+});
+
+
+
+/*
+async function fetchImg(url) {
     return fetch(url, {mode: 'cors'})
     .then(response => response.json())
     .then(response => img.src = response.data.images.original.url);
@@ -28,3 +58,5 @@ goBtn.addEventListener('click', () => {
         alert(error)
     });
 });
+
+*/
